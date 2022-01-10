419de9799b5cfee3c3d80a29f00c5286b72bb246
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-01-10 17:22:34 -0300

    Requisito 1, Requisito 2

---
+++ src/pages/Login.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import validator from 'validator';
+import { sendLogin } from '../actions';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      senha: '',
+      buttonValidate: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    const { email, senha } = this.state;
+    let { buttonValidate } = this.state;
+    const { dispatchEmail } = this.props;
+    const numberMaxPassword = 6;
+    if (validator.isEmail(email) && senha.length === numberMaxPassword) {
+      buttonValidate = false;
+    }
+    return (
+      <div>
+        <h1>Login</h1>
+        <input
+          type="email"
+          name="email"
+          value={ email }
+          placeholder="email"
+          data-testid="email-input"
+          onChange={ this.handleChange }
+        />
+        <input
+          name="senha"
+          type="password"
+          value={ senha }
+          placeholder="senha"
+          data-testid="password-input"
+          onChange={ this.handleChange }
+        />
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ buttonValidate }
+            onClick={ () => dispatchEmail(email) }
+          >
+            Entrar
+          </button>
+        </Link>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchEmail: (email) => dispatch(sendLogin(email)),
+});
+
+Login.propTypes = {
+  dispatchEmail: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
